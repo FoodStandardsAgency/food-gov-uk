ff9545d00ff4a8ce75c0b2596f4e7f2ce47aa5f0
Luc Bezier <luc@webizat.com>
2014-02-14 17:51:14 +0800

    Added script directory with full-rebuilt and oracle-test

---
+++ scripts/general/full-rebuild.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DATE=$(date | tr ' ' '-' | tr ':' '-')
+
+echo "${DATE}"
+
+cd ../../docroot/sites/default
+
+drush -v -y si --site-name='Food Standards Agency' --account-name='admin' --account-pass='fsa' base
+
+# @TODO add vocabulary terms.
+
+# Open the ssh tunnel.
+setup-ssh-tunnel.sh &
+
+# Sleep for 30 seconds to allow for the tunnel to be built.
+sleep 30
+
+# Migrate all users.
+
+# Migrate content.
+
+drush cc all
+
+drush -y vset "bbcgf_migration_date" "${DATE}"
