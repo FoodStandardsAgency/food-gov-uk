b50e6011af17b46480de9a95985d6817016ca31b
Robert Castelo <robert@codepositive.com>
2014-03-13 19:19:01 +0000

    FSA-351 set up main site search

--- docroot/sites/all/themes/site_frontend/template.php
+++ docroot/sites/all/themes/site_frontend/template.php
@@ -5,3 +5,40 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * Site Frontend theme.
  */
+
+
+/**
+ * Returns HTML for an inactive facet item.
+ * Customised to make active filters easier to remove.
+ *
+ * @param $variables
+ *   An associative array containing the keys 'text', 'path', and 'options'. See
+ *   the l() function for information about these variables.
+ *
+ * @see l()
+ *
+ * @ingroup themeable
+ */
+function site_frontend_facetapi_link_active($variables) {
+
+  // Sanitizes the link text if necessary.
+  $sanitize = empty($variables['options']['html']);
+  $link_text = ($sanitize) ? check_plain($variables['text']) : $variables['text'];
+
+  // Theme function variables fro accessible markup.
+  // @see http://drupal.org/node/1316580
+  $accessible_vars = array(
+    'text' => $variables['text'],
+    'active' => TRUE,
+  );
+
+  // Builds link, passes through t() which gives us the ability to change the
+  // position of the widget on a per-language basis.
+  $replacements = array(
+    '!facetapi_accessible_markup' => theme('facetapi_accessible_markup', $accessible_vars),
+  );
+  $variables['text'] = $link_text . ' <span class="apachesolr-unclick-decoration">&nbsp;x</span>' . t('!facetapi_accessible_markup', $replacements);
+  $variables['options']['attributes']['class'] = array('apachesolr-unclick');
+  $variables['options']['html'] = TRUE;
+  return theme_link($variables);
+}
