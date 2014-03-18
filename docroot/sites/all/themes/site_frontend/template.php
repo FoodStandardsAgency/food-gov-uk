03c2956d45461851287d07b55f8807a868ef0730
Tyler Collins <tyler@codepositive.com>
2014-03-18 15:17:48 +0000

    implemented changes for username function

--- docroot/sites/all/themes/site_frontend/template.php
+++ docroot/sites/all/themes/site_frontend/template.php
@@ -43,30 +43,6 @@ function site_frontend_facetapi_link_active($variables) {
   return theme_link($variables);
 }
 
-  /**
-   * Helper function returns who the current user is.
-   *
-   * There is no good solution for this with standard Drupal.
-   *
-   * The following HTML should be added to the footer:
-   *
-   * @code
-   *   <div class="username"><!--username--></div>
-   * @endcode
-   *
-   * One solution is to do this in template.php:
-   *
-   * @code
-   */
-   function site_frontend_preprocess_page(&$vars) {
-      if (user_is_logged_in()) {
-        $vars['page']['footer']['username'] = array(
-        '#prefix' => '<div class="username"><!--',
-        '#markup' => check_plain($GLOBALS['user']->name),
-        '#suffix' => '--></div>',
-    );
-  }
-}
 
 
 
