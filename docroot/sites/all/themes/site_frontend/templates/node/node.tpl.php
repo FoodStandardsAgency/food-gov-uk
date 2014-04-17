ac87ac1955d5a948230c872eade915b32fce8043
Anthony Albertyn <anthony@binarybrand.com>
2014-04-17 17:42:26 +0100

    Remove print rendering of fields from node.tpl.php

--- docroot/sites/all/themes/site_frontend/templates/node/node.tpl.php
+++ docroot/sites/all/themes/site_frontend/templates/node/node.tpl.php
@@ -104,10 +104,6 @@
       // Add new region for certain content types for full page view mode 
       $summary_types = array('document_page');
 	  if (in_array($type, $summary_types) &&  $view_mode == 'full' ) {
-	    // print fields here
-        print render($content['field_updated']);
-		print render($content['field_feature_image']);
-	    print render($content['field_summary']);
 	    // insert our core region
 	    print render($region_node_inline);
 	  }
