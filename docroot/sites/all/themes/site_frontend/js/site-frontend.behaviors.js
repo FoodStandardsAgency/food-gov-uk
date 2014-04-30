af9b8e0588bfb543ee759b5c6739f411a22ae10a
greenman <greenman@codepositive.com>
2014-04-30 17:55:30 +0100

    Remove label invisible class from the search box

--- docroot/sites/all/themes/site_frontend/js/site-frontend.behaviors.js
+++ docroot/sites/all/themes/site_frontend/js/site-frontend.behaviors.js
@@ -57,4 +57,18 @@
     }
   };
 
+  Drupal.behaviors.siteFrontendSearchLabel = {
+        attach: function (context, settings) {
+            // By using the 'context' variable we make sure that our code only runs on
+            // the relevant HTML. Furthermore, by using jQuery.once() we make sure that
+            // we don't run the same piece of code for an HTML snippet that we already
+            // processed previously. By using .once('foo') all processed elements will
+            // get tagged with a 'foo-processed' class, causing all future invocations
+            // of this behavior to ignore them.
+            $('#search-block-form label.element-invisible', context).removeClass('element-invisible');
+
+        }
+    };
+
+
 })(jQuery);
