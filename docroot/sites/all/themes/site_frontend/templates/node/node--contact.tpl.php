5b21ce7798eec198f15192ac3c5b3658aafbc387
Tyler Collins <tyler@codepositive.com>
2014-07-07 18:06:58 +0100

    updated template - node--contact

--- docroot/sites/all/themes/site_frontend/templates/node/node--contact.tpl.php
+++ docroot/sites/all/themes/site_frontend/templates/node/node--contact.tpl.php
@@ -80,7 +80,7 @@
 
 ?>
 <article<?php print $attributes; ?>>
-  <h4<?php print $title_attributes; ?><?php print $title; ?></h4>
+  <h4<?php print $title_attributes; ?>><?php print $title; ?></h4>
   <div<?php print $content_attributes; ?>>
     <?php
       // We hide the comments and links now so that we can render them later.
