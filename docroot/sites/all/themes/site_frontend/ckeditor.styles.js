fbeca1e079698c2d14822ebd5724723af1fa4f77
Robert Castelo <robert@codepositive.com>
2014-07-07 16:28:19 +0100

    FSA-753: Review CKeditor js and css style sheet settings (https://codepositive.atlassian.net/browse/FSA-753)

--- docroot/sites/all/themes/site_frontend/ckeditor.styles.js
+++ docroot/sites/all/themes/site_frontend/ckeditor.styles.js
@@ -19,10 +19,10 @@ if (typeof(CKEDITOR) !== 'undefined') {
 
       /* Inline Styles */
 			// { name: 'Text align: Left', element: 'p', attributes: { 'class': 'text-align-left' } },
-      { name: 'Section', element: 'h2', attributes: { 'class': 'inline-section-heading' } },
       { name: 'Guidance', element: 'div', attributes: { 'class': 'guidance' } },
       { name: 'Training', element: 'div', attributes: { 'class': 'training' } },
       { name: 'Science', element: 'div', attributes: { 'class': 'science moreInfo' } },
+      { name: 'Section', element: 'h2', attributes: { 'class': 'inline-section-heading' } },
 
 
       /* Object Styles */
