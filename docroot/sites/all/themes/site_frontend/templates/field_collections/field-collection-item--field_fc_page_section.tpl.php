d71b2ad388558b7b89a086a0530c1661370955de
greenman <greenman@codepositive.com>
2014-07-13 15:42:48 +0100

    Correct alert file. not displaying science data if there is none.

--- docroot/sites/all/themes/site_frontend/templates/field_collections/field-collection-item--field_fc_page_section.tpl.php
+++ docroot/sites/all/themes/site_frontend/templates/field_collections/field-collection-item--field_fc_page_section.tpl.php
@@ -54,15 +54,19 @@
      hide($content['field_fc_section_quote']);
    }
 
+  if (
+    isset($field_collection_item->field_science_data) &&
+    isset($field_collection_item->field_science_data[$language][0]) &&
+    !$field_collection_item->field_science_data[$language][0]['value']) {
+    hide($content['field_science_data']);
+  }
 
   // Create image and wrapper markup.
 
   if (!empty($field_collection_item->field_fc_image_caption)) {
-		
       // Remove image caption field from content so we can render it separately,
       // if there is no image, it won't render at all
       hide($content['field_fc_image_caption']);
-
       // Determine the image caption.
       $image_caption = $field_collection_item->field_fc_image_caption[$language][0]['value'];
   }
