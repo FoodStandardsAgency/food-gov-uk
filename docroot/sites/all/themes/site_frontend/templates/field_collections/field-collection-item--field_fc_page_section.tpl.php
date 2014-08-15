e3aa68a3402b473915c5e98a1f2d9d66c250ed6a
greenman <greenman@codepositive.com>
2014-08-15 09:47:33 +0100

    Removed short form php tags

--- docroot/sites/all/themes/site_frontend/templates/field_collections/field-collection-item--field_fc_page_section.tpl.php
+++ docroot/sites/all/themes/site_frontend/templates/field_collections/field-collection-item--field_fc_page_section.tpl.php
@@ -29,166 +29,168 @@
  */
 
 
+$image = '';
+$image_position = '';
+$image_caption = '';
+$image_with_wrapper = '';
 
+// Determine the language.
+$language = isset($field_collection_item->lancode) ? $field_collection_item->lancode : 'und';
 
-  $image = '';
-  $image_position = '';
-  $image_caption = '';
-  $image_with_wrapper = '';
+// Load parent node so that we can check settings
+$node = menu_get_object();
 
-  // Determine the language.
-  $language = isset($field_collection_item->lancode) ? $field_collection_item->lancode : 'und';
-
-  // Load parent node so that we can check settings
-  $node = menu_get_object();
-
-  // get back to top setting from parent node
-  $back_to_top = field_get_items('node', $node, 'field_setting_backtotop');
-  $back_to_top = $back_to_top[0]['value'];
+// get back to top setting from parent node
+$back_to_top = field_get_items('node', $node, 'field_setting_backtotop');
+$back_to_top = $back_to_top[0]['value'];
 
 // check for empty quote (a migration artifact)
-   if (
-     isset($field_collection_item->field_fc_section_quote) &&
-     isset($field_collection_item->field_fc_section_quote[$language][0]['value']) &&
-     !$field_collection_item->field_fc_section_quote[$language][0]['value']) {
-     hide($content['field_fc_section_quote']);
-   }
+if (
+  isset($field_collection_item->field_fc_section_quote) &&
+  isset($field_collection_item->field_fc_section_quote[$language][0]['value']) &&
+  !$field_collection_item->field_fc_section_quote[$language][0]['value']
+) {
+  hide($content['field_fc_section_quote']);
+}
 
-  if (
-    isset($field_collection_item->field_science_data) &&
-    isset($field_collection_item->field_science_data[$language][0]) &&
-    !$field_collection_item->field_science_data[$language][0]['value']) {
-    hide($content['field_science_data']);
+if (
+  isset($field_collection_item->field_science_data) &&
+  isset($field_collection_item->field_science_data[$language][0]) &&
+  !$field_collection_item->field_science_data[$language][0]['value']
+) {
+  hide($content['field_science_data']);
+}
+
+// Create image and wrapper markup.
+
+if (!empty($field_collection_item->field_fc_image_caption)) {
+  // Remove image caption field from content so we can render it separately,
+  // if there is no image, it won't render at all
+  hide($content['field_fc_image_caption']);
+  // Determine the image caption.
+  $image_caption = $field_collection_item->field_fc_image_caption[$language][0]['value'];
+}
+
+if (!empty($field_collection_item->field_fc_section_image)) {
+
+  // Remove image field from content so we can render it separately.
+  hide($content['field_fc_section_image']);
+
+  // Determine the image position.
+  $image_position = empty($field_collection_item->field_fc_image_position) ? 'img-pos-right' : $field_collection_item->field_fc_image_position[$language][0]['value'];
+
+  // Determine which image style to use.
+  if ($image_position == 'img-pos-top' || $image_position == 'img-pos-bottom') {
+    $image_style = 'section_top_bottom';
+  }
+  else {
+    $image_style = 'page_section__left_right_';
   }
 
-  // Create image and wrapper markup.
+  // Create the image markup.
+  $image = theme('image_style',
+    array(
+      'style_name' => $image_style,
+      'path' => $field_collection_item->field_fc_section_image[$language][0]['uri'],
+      'alt' => $field_collection_item->field_fc_section_image[$language][0]['alt'],
+      'title' => $field_collection_item->field_fc_section_image[$language][0]['title'],
+      'attributes' => array('class' => $image_position),
+    )
+  );
 
-  if (!empty($field_collection_item->field_fc_image_caption)) {
-      // Remove image caption field from content so we can render it separately,
-      // if there is no image, it won't render at all
-      hide($content['field_fc_image_caption']);
-      // Determine the image caption.
-      $image_caption = $field_collection_item->field_fc_image_caption[$language][0]['value'];
+  // Wrap the image, and add the caption (if available).
+  if (!empty($image_caption)) {
+    $image_with_wrapper = '<div class="wrapper-' . $image_position . ' with-caption">';
+    $image_with_wrapper .= $image;
+    $image_with_wrapper .= '<div class="image-caption">' . $image_caption . '</div>';
+    $image_with_wrapper .= '</div>';
+  }
+  else {
+    $image_with_wrapper = '<div class="wrapper-' . $image_position . ' without-caption">' . $image . '</div>';
   }
 
-  if (!empty($field_collection_item->field_fc_section_image)) {
-
-    // Remove image field from content so we can render it separately.
-    hide($content['field_fc_section_image']);
-
-    // Determine the image position.
-    $image_position = empty($field_collection_item->field_fc_image_position) ? 'img-pos-right' : $field_collection_item->field_fc_image_position[$language][0]['value'];
-
-    // Determine which image style to use.
-    if ($image_position == 'img-pos-top' || $image_position == 'img-pos-bottom') {
-      $image_style = 'section_top_bottom';
-    }
-    else {
-      $image_style = 'page_section__left_right_';
-    }
-
-    // Create the image markup.
-    $image = theme('image_style',
-      array(
-        'style_name' => $image_style,
-        'path' => $field_collection_item->field_fc_section_image[$language][0]['uri'],
-        'alt' => $field_collection_item->field_fc_section_image[$language][0]['alt'],
-        'title' => $field_collection_item->field_fc_section_image[$language][0]['title'],
-        'attributes' => array('class' => $image_position),
-      )
-    );
-
-    // Wrap the image, and add the caption (if available).
-    if(!empty($image_caption)) {
-	    $image_with_wrapper = '<div class="wrapper-' . $image_position .' with-caption">';
-	    $image_with_wrapper .= $image;
-	    $image_with_wrapper .= '<div class="image-caption">' . $image_caption . '</div>';
-	    $image_with_wrapper .= '</div>';
-	} else {
-	    $image_with_wrapper = '<div class="wrapper-' . $image_position .' without-caption">' . $image . '</div>';	
-	}
-
-  }  // end --- Create image and wrapper markup.
+} // end --- Create image and wrapper markup.
 
 
-  // Related items.
-  // Heading will display even if there's no content, so hide it here.
-  hide($content['field_fc_related_items_heading']);
+// Related items.
+// Heading will display even if there's no content, so hide it here.
+hide($content['field_fc_related_items_heading']);
 
-  if (!empty($field_collection_item->field_fc_related_items)) {
-    $related_items_heading = render($content['field_fc_related_items_heading']);
-    $related_item = render($content['field_fc_related_items']);
-	$related_items_wrapper = '<div class="related-items-wrapper"><div class="related-items-inner">';
-	$related_items_wrapper .= $related_items_heading;
-	$related_items_wrapper .= $related_item;
-	$related_items_wrapper .= '</div></div>';
-  }
+if (!empty($field_collection_item->field_fc_related_items)) {
+  $related_items_heading = render($content['field_fc_related_items_heading']);
+  $related_item = render($content['field_fc_related_items']);
+  $related_items_wrapper = '<div class="related-items-wrapper"><div class="related-items-inner">';
+  $related_items_wrapper .= $related_items_heading;
+  $related_items_wrapper .= $related_item;
+  $related_items_wrapper .= '</div></div>';
+}
 
-  // CSV files
-  // Heading will display even if there's no content, so hide it here.
-  hide($content['field_fc_files_csv_heading']);
+// CSV files
+// Heading will display even if there's no content, so hide it here.
+hide($content['field_fc_files_csv_heading']);
 
-  if (!empty($field_collection_item->field_fc_files_csv)) {
-    $csv_files_heading = render($content['field_fc_files_csv_heading']);
-    $csv_files = render($content['field_fc_files_csv']);
-	$csv_wrapper_header_files = '<div class="csv-files-wrapper"><div class="csv-files-inner">';
-	$csv_wrapper_header_files .= $csv_files_heading;
-	$csv_wrapper_header_files .= $csv_files;
-	$csv_wrapper_header_files .= '</div></div>';
-  }
+if (!empty($field_collection_item->field_fc_files_csv)) {
+  $csv_files_heading = render($content['field_fc_files_csv_heading']);
+  $csv_files = render($content['field_fc_files_csv']);
+  $csv_wrapper_header_files = '<div class="csv-files-wrapper"><div class="csv-files-inner">';
+  $csv_wrapper_header_files .= $csv_files_heading;
+  $csv_wrapper_header_files .= $csv_files;
+  $csv_wrapper_header_files .= '</div></div>';
+}
 
-  // Child pages.
-  // Heading will display even if there's no content, so hide it here.
-  hide($content['field_child_pages_heading']);
+// Child pages.
+// Heading will display even if there's no content, so hide it here.
+hide($content['field_child_pages_heading']);
 
-  if (!empty($field_collection_item->field_child_page)) {
-    $child_pages_heading = render($content['field_child_pages_heading']);
-    $child_page = render($content['field_child_page']);
-  }
+if (!empty($field_collection_item->field_child_page)) {
+  $child_pages_heading = render($content['field_child_pages_heading']);
+  $child_page = render($content['field_child_page']);
+}
 
 ?>
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <div class="content"<?php print $content_attributes; ?>>
     <?php
-      if (isset($content['field_fc_section_heading'])
-        and trim($content['field_fc_section_heading']['#items'][0]['value'])){
-        print render($content['field_fc_section_heading']);
-      }
-      else {
-        // there is no value here, so don't display an empty heading
-        hide($content['field_fc_section_heading']);
-      }
+    if (isset($content['field_fc_section_heading'])
+      and trim($content['field_fc_section_heading']['#items'][0]['value'])
+    ) {
+      print render($content['field_fc_section_heading']);
+    }
+    else {
+      // there is no value here, so don't display an empty heading
+      hide($content['field_fc_section_heading']);
+    }
 
 
-      if ($image_position != 'img-pos-bottom') {
-        print $image_with_wrapper;
-      }
+    if ($image_position != 'img-pos-bottom') {
+      print $image_with_wrapper;
+    }
 
-      print render($content);
+    print render($content);
 
-      if ($image_position == 'img-pos-bottom') {
-        print $image_with_wrapper;
-      }
+    if ($image_position == 'img-pos-bottom') {
+      print $image_with_wrapper;
+    }
 
-      if (isset($related_item)) {
-        print $related_items_wrapper;
-      }
+    if (isset($related_item)) {
+      print $related_items_wrapper;
+    }
 
-	  if (isset($csv_files)) {
-		print $csv_wrapper_header_files;
-      }
+    if (isset($csv_files)) {
+      print $csv_wrapper_header_files;
+    }
 
-      if (isset($child_page)) {
-        print $child_pages_heading;
-        print $child_page;
-      }
+    if (isset($child_page)) {
+      print $child_pages_heading;
+      print $child_page;
+    }
 
     ?>
-    <?php  if ($back_to_top) {  ?>
+    <?php if ($back_to_top) : ?>
       <div class="section-back-top">
-         <a href="#main-content"><?=t('Back to top');?> </a>
+        <a href="#main-content"><? print t('Back to top'); ?> </a>
       </div>
-      <? } ?>
+    <? endif; ?>
 
   </div>
 </div>
