b05a5b133d797797bbf7bcf2cd0043cff523a14f
Robert Castelo <robert@codepositive.com>
2014-10-22 14:42:19 +0100

    FSA-810: PDF icons in related content fail multiple accessibility counts (https://codepositive.atlassian.net/browse/FSA-810)

--- docroot/sites/all/themes/site_frontend/template.php
+++ docroot/sites/all/themes/site_frontend/template.php
@@ -67,35 +67,3 @@ function site_frontend_links__locale_block(&$vars) {
 }
 
 
-/**
- * Implements hook_file_link
- * Add a link to the file icon
- */
-
-function site_frontend_file_link($variables) {
-  $file = $variables['file'];
-  $icon_directory = $variables['icon_directory'];
-
-  $url = file_create_url($file->uri);
-  $icon = theme('file_icon', array('file' => $file, 'icon_directory' => $icon_directory));
-
-  // Set options as per anchor format described at
-  // http://microformats.org/wiki/file-format-examples
-  $options = array(
-    'attributes' => array(
-      'type' => $file->filemime . '; length=' . $file->filesize,
-    ),
-  );
-
-  // Use the description as the link text if available.
-  if (empty($file->description)) {
-    $link_text = $file->filename;
-  }
-  else {
-    $link_text = $file->description;
-    $options['attributes']['title'] = check_plain($file->filename);
-  }
-  // Edit - add link with l() to icon
-  return '<span class="file">' . l($icon, $url, array('html' => TRUE)) . ' ' . l($link_text, $url, $options) . '</span>';
-}
-
