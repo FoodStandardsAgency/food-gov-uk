f1a6bd703cf718d6f90c9f7ecb16cdf20d796ff1
Anthony Albertyn <anthony@binarybrand.com>
2014-10-27 23:38:56 +0000

    Add FAQ expand contract feature new CSS file and existing JS to local custom module and add html markup to new FAQ node template,

--- docroot/sites/all/modules/custom/local/js/local.js
+++ docroot/sites/all/modules/custom/local/js/local.js
@@ -11,7 +11,20 @@
         active: false
       });
 
+      // Expand FAQ accordion.
+      $("#expand").click(function(){
+        $('#expand').removeClass('show');
+        $('#collapse').addClass('show');
+        $('.ui-accordion-content').slideDown ('normal');
+      });
+
+      // Collapse FAQ accordion.
+      $("#collapse").click(function(){
+        $('#collapse').removeClass('show');
+        $('#expand').addClass('show');
+        $('.ui-accordion-content').slideUp ('normal');
+      });
+
     }
   };
 })(jQuery);
-
