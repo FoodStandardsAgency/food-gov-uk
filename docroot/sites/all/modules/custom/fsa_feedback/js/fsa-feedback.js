98275ea64e202811f0ed7ed9a22abd2b20256c4f
Matt Farrow <matt.farrow@siriusopensource.com>
2015-04-10 16:09:34 +0100

    Add custom JS file for feedback
    * Added a new custom JS file
    * Attached the JS file in hook_page_build()
    * Bind open/close event to add/remove class no-border
    
    [ Partial fix for #2015040810000011 ]

---
+++ docroot/sites/all/modules/custom/fsa_feedback/js/fsa-feedback.js
@@ -0,0 +1,39 @@
+(function ($) {
+
+  Drupal.behaviors.fsaFeedbackForm = {
+    attach: function (context) {
+      $('#block-feedback-form', context).once('fsafeedback', function () {
+        var $block = $(this);
+        var $formContainer = $block.find('.grey-container-block');
+        $formContainer.addClass('no-border');
+        $block.find('span.feedback-link').toggle(function () {
+              Drupal.fsaFeedbackFormToggle($block, false);
+            },
+            function() {
+              Drupal.fsaFeedbackFormToggle($block, true);
+            }
+          );
+      });
+    }
+  };
+
+  /**
+   * Collapse or uncollapse the feedback form block.
+   */
+  Drupal.fsaFeedbackFormToggle = function ($block, enable) {
+    //$block.find('form').slideToggle('medium');
+    
+    var $formContainer = $block.find('.grey-container-block');
+    
+    if (enable) {
+      //$('#feedback-form-toggle', $block).html('[ + ]');
+      $formContainer.addClass('no-border');
+    }
+    else {
+      //$('#feedback-form-toggle', $block).html('[ &minus; ]');
+      console.log(enable);
+      $formContainer.removeClass('no-border');
+    }
+  };
+
+})(jQuery);
