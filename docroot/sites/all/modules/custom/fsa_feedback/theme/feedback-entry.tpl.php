081e0463ee631397660e594dc1972a2c7e8295dc
Matt Farrow <matt.farrow@siriusopensource.com>
2015-04-14 17:50:05 +0100

    Add styling improvements to feeback admin UI
    Various styling tweaks to the feedback admin user
    interface, including:
    
    * Table vertical alignment in feedback listing
    * Adding the custom CSS to the list and detail view
    * Cleaning up the detail view with tempalate and CSS changes
    
    [ Partial fix for #2015040810000011 ]

--- docroot/sites/all/modules/custom/fsa_feedback/theme/feedback-entry.tpl.php
+++ docroot/sites/all/modules/custom/fsa_feedback/theme/feedback-entry.tpl.php
@@ -8,24 +8,30 @@
 ?>
 <div class="feedback-entry"<?php print $attributes; ?>>
   
-  <div class="field">
-    <div class="field-label"><?php print t('Location'); ?>:&nbsp;</div>
-    <?php print $location; ?>
-  </div>
-  <div class="field">
-    <div class="field-label"><?php print t('Date'); ?>:&nbsp;</div>
-    <?php print $date; ?>
-  </div>
-  <div class="field">
-    <div class="field-label"><?php print t('User'); ?>:&nbsp;</div>
-    <?php print $account; ?>
-  </div>
- 
-  <?php print render($content); ?>
+  <div class="feedback-entry-detail">
   
-  <div class="field">
-    <strong>Browser: </strong>
-    <?php print render($browser); ?>
+    <div class="field field-label-inline">
+      <div class="field-label"><?php print t('Location'); ?>:&nbsp;</div>
+      <?php print $location; ?>
+    </div>
+    <div class="field field-label-inline">
+      <div class="field-label"><?php print t('Date'); ?>:&nbsp;</div>
+      <?php print $date; ?>
+    </div>
+
+    <div class="field field-label-inline">
+      <div class="field-label"><?php print t('User'); ?>:&nbsp;</div>
+      <?php print $account; ?>
+    </div>
+
+    <div class="field field-label-inline">
+      <div class="field-label">Browser:&nbsp;</div>
+      <?php print render($browser); ?>
+    </div>
+    
   </div>
+  
+  <h2>Message</h2>
+  <?php print render($content); ?>
   
 </div>
