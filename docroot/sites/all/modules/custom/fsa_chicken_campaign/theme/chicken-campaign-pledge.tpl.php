ec1665342224a540ab2f9b1cf3206cc06363bc99
Matt Farrow <matt.farrow@siriusopensource.com>
2015-04-30 17:42:35 +0100

    Create chicken campaign module
    Created a new module for the Chicken campaign.
    
    It provides blocks for adding to the pages via context,
    as well as block-specific CSS and JavaScript.

---
+++ docroot/sites/all/modules/custom/fsa_chicken_campaign/theme/chicken-campaign-pledge.tpl.php
@@ -0,0 +1,58 @@
+<div class="cc-container">
+  <div class="cc-header">
+    <div class="cc-pledge-container">
+      <div class="cc-pledge">
+        <div class="cc-pledge-img"><a class="popup-open-trigger" href="#"><img src="//staging.food.gov.uk/sites/default/files/chicken-challenge-logo.png" alt="The 2015 Chicken challenge"/></a></div>
+        <div class="cc-pledge-description">
+          <p>We promise to do our bit to cut campylobacter food poisoning in half</p>
+          <button class="cc-button popup-open-trigger" type="button">Count me in!</button>
+        </div>
+        <div class="cc-totalizer">
+          <div class="cc-totalizer-number"><div><iframe id="totalizer" title="Totaliser" src="//www.chickenchallengetotaliser.co.uk/ShowTotalCount.aspx"></iframe></div></div>
+          <p>Supporters</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="cc-popup-container">
+
+  <div class="cc-popup cc-popup-before-submit">
+    <button class="cc-close-popup popup-close-trigger">x</button>
+    <form id="popupForm" action="#">
+      <p>Thanks for stepping up and being counted...</p>
+      <p>Spread the word, not the germs. Invite your friends to be counted too. It's easy!</p>
+      <a id="shareTrigger" class="cc-button" href="#">Share</a>
+      <a class="addthis_button cc-hidden" href="http://www.addthis.com/bookmark.php"></a>
+    </form>
+  </div>
+
+  <div class="cc-popup cc-popup-after-submit">
+    <button class="cc-close-popup popup-close-trigger">x</button>
+    <h2>You have already submitted your pledge.</h2>
+  </div>
+
+</div>
+
+
+<!-- Add this share code -->
+<script type="text/javascript">
+  var addthis_config = addthis_config || {};
+  addthis_config.data_track_clickback = false;
+  var addthis_share = {
+    url: "http://www.food.gov.uk/chickenchallenge",
+    title: "I’ve promised to do my bit to cut campylobacter food poisoning in half. Are you up for the #ChickenChallenge?"
+  }
+</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://code.jquery.com/jquery-migrate-1.0.0.js"></script>
+
+<!--[if lte IE 9]>
+    <script src=" http://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxtransport-xdomainrequest/1.0.3/jquery.xdomainrequest.min.js"></script>
+<![endif]-->
+
+<!--[if lte IE 8]>
+    <script src=" http://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxtransport-xdomainrequest/1.0.3/jquery.xdomainrequest.min.js"></script>
+    <![endif]-->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-538f34a90a1d7c01" async="async"></script>
---
+++ docroot/sites/all/modules/custom/fsa_chicken_campaign/theme/chicken-campaign-tint-board.tpl.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @file
+ * Default theme implementation for a tint board.
+ */
+?>
+<script async src="https://d36hc0p18k1aoc.cloudfront.net/pages/a5b5e4.js"></script>
+<div class="a5b5e4" data-id="test2015" data-columns="" data-expand="true" data-infinitescroll="true" style="height:500px;width:100%;"></div>
