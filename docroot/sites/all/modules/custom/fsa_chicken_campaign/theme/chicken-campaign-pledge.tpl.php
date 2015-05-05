a37f64496a6b9fe0c95854a9a3f4e7dab961e73b
Matt Farrow <matt.farrow@siriusopensource.com>
2015-05-05 15:02:39 +0100

    Make pledge translatable
    In the Chicken Challenge pledge block, made the text translatable
    using the `t()` function.
    
    Also replaced all links with protocol-relative versions.

--- docroot/sites/all/modules/custom/fsa_chicken_campaign/theme/chicken-campaign-pledge.tpl.php
+++ docroot/sites/all/modules/custom/fsa_chicken_campaign/theme/chicken-campaign-pledge.tpl.php
@@ -4,12 +4,12 @@
       <div class="cc-pledge">
         <div class="cc-pledge-img"><a class="popup-open-trigger" href="#"><img src="//staging.food.gov.uk/sites/default/files/chicken-challenge-logo.png" alt="The 2015 Chicken challenge"/></a></div>
         <div class="cc-pledge-description">
-          <p>We promise to do our bit to cut campylobacter food poisoning in half</p>
-          <button class="cc-button popup-open-trigger" type="button">Count me in!</button>
+          <p><?php print t('We promise to do our bit to cut campylobacter food poisoning in half'); ?></p>
+          <button class="cc-button popup-open-trigger" type="button"><?php print t('Count me in'); ?>!</button>
         </div>
         <div class="cc-totalizer">
           <div class="cc-totalizer-number"><div><iframe id="totalizer" title="Totaliser" src="//www.chickenchallengetotaliser.co.uk/ShowTotalCount.aspx"></iframe></div></div>
-          <p>Supporters</p>
+          <p><?php print t('Supporters'); ?></p>
         </div>
       </div>
     </div>
@@ -21,8 +21,8 @@
   <div class="cc-popup cc-popup-before-submit">
     <button class="cc-close-popup popup-close-trigger">x</button>
     <form id="popupForm" action="#">
-      <p>Thanks for stepping up and being counted...</p>
-      <p>Spread the word, not the germs. Invite your friends to be counted too. It's easy!</p>
+      <p><?php print t('Thanks for stepping up and being counted'); ?>...</p>
+      <p><?php print t("Spread the word, not the germs. Invite your friends to be counted too. It's easy!"); ?></p>
       <a id="shareTrigger" class="cc-button" href="#">Share</a>
       <a class="addthis_button cc-hidden" href="http://www.addthis.com/bookmark.php"></a>
     </form>
@@ -30,7 +30,7 @@
 
   <div class="cc-popup cc-popup-after-submit">
     <button class="cc-close-popup popup-close-trigger">x</button>
-    <h2>You have already submitted your pledge.</h2>
+    <h2><?php print t('You have already submitted your pledge'); ?>.</h2>
   </div>
 
 </div>
@@ -45,14 +45,14 @@
     title: "I’ve promised to do my bit to cut campylobacter food poisoning in half. Are you up for the #ChickenChallenge?"
   }
 </script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="http://code.jquery.com/jquery-migrate-1.0.0.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.0.0.js"></script>
 
 <!--[if lte IE 9]>
-    <script src=" http://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxtransport-xdomainrequest/1.0.3/jquery.xdomainrequest.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-ajaxtransport-xdomainrequest/1.0.3/jquery.xdomainrequest.min.js"></script>
 <![endif]-->
 
 <!--[if lte IE 8]>
-    <script src=" http://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxtransport-xdomainrequest/1.0.3/jquery.xdomainrequest.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-ajaxtransport-xdomainrequest/1.0.3/jquery.xdomainrequest.min.js"></script>
     <![endif]-->
 <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-538f34a90a1d7c01" async="async"></script>
