31e84bcaba456190fc18992a8b548017513572d6
Matt Farrow <matt.farrow@siriusopensource.com>
2015-05-15 13:19:35 +0100

    Remove font scale links from accessibility template
    Because the text size buttons work only when JavaScript is present
    and activated, there's little point in displaying the links if this
    is not the case.
    
    Consequently, instead of coding the HTML for the links in the template,
    we'll add these in via JavaScript.
    
    This is progressive enhancement in that it adds functionality where
    the user agent is capable of making use of it. If we were to leave
    the links in, it would get confusing for any users who don't have
    JavaSript operational.
    
    [ Partial fix for #1030 ]

--- docroot/sites/all/modules/custom/accessibility_links/theme/accessibility-links.tpl.php
+++ docroot/sites/all/modules/custom/accessibility_links/theme/accessibility-links.tpl.php
@@ -1,10 +1,6 @@
 <div id="accessTools">
   <ul>
     <li class="first"><a href="<?php print $accessibility_page_url; ?>"><?php print t('Accessibility'); ?></a></li>
-    <li id="fontScale">
-      <a id="fontScale0" class="fontScalePlus0" href="#" title="Set font size to default">A</a>
-      <a id="fontScale1" class="fontScalePlus1" href="#" title="Set font size to +1">A</a>
-      <a id="fontScale2" class="fontScalePlus2" href="#" title="Set font size to +2">A</a>
-    </li>
+    <li id="fontScale"></li>
   </ul>
 </div>
