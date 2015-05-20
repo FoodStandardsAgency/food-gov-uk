6857aa59966e7f8529de1b79c92e1b04673b4032
Josh Holland <josh.holland@siriusopensource.com>
2015-05-20 15:13:59 +0100

    Update of Behat configuration

--- tests/features/bootstrap/FeatureContext.php
+++ tests/features/bootstrap/FeatureContext.php
@@ -1,72 +1,23 @@
 <?php
 
-use Behat\Behat\Context\ClosuredContextInterface,
-    Behat\Behat\Context\TranslatedContextInterface,
-    Behat\Behat\Context\BehatContext,
-    Behat\Behat\Exception\PendingException;
-use Behat\Gherkin\Node\PyStringNode,
-    Behat\Gherkin\Node\TableNode;
-
-//
-// Require 3rd-party libraries here:
-//
-//   require_once 'PHPUnit/Autoload.php';
-//   require_once 'PHPUnit/Framework/Assert/Functions.php';
-//
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
 
 /**
- * Features context.
+ * Defines application features from the specific context.
  */
-class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext
-{
-    /**
-     * Initializes context.
-     * Every scenario gets it's own context object.
-     *
-     * @param array $parameters context parameters (set them up through behat.yml)
-     */
-    public function __construct(array $parameters) 
-    {  
-        // Initialize your context here
-        $this->parameters = $parameters;
-    }
+class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext {
 
-    
-//
-// Place your definition and hook methods here:
-//
-//    /**
-//     * @Given /^I have done something with "([^"]*)"$/
-//     */
-//    public function iHaveDoneSomethingWith($argument)
-//    {
-//        doSomethingWith($argument);
-//    }
-//
- /**
-  * @Given /^I wait (\d+) seconds(?:|s)$/
-  */
-  public function iWaitSeconds($seconds) {
-    $this->getSession()->wait($seconds*1000);  
-  }
-  
-   /**
-   * If the website is access protected with HTTP basic auth,
-   * we perform an authentication before each scenario with the credentials
-   * from the configuration
+  /**
+   * Initializes context.
    *
-   * @BeforeScenario
+   * Every scenario gets its own context instance.
+   * You can also pass arbitrary arguments to the
+   * context constructor through behat.yml.
    */
-  
-  public function performBasicHttpAuthentication($event) {
-    $driver = $this->getSession()->getDriver();
-    // only use this with drivers that support it
-    if ($driver instanceof Behat\Mink\Driver\GoutteDriver) {
-  
-        $this->getSession()->setBasicAuth(
-          $this->parameters['authentication']['username'],
-          $this->parameters['authentication']['password']
-        );
-    } 
-   }
- }
+  public function __construct() {
+  }
+
+}
