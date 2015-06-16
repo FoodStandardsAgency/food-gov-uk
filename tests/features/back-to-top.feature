6aa881e82b128450c0f4be966737c797ac553860
Josh Holland <josh.holland@siriusopensource.com>
2015-06-16 10:57:38 +0100

    [behat] Initial working test suite

--- tests/features/back-to-top.feature
+++ tests/features/back-to-top.feature
@@ -3,111 +3,20 @@ Feature: Back to top links
   As a Content Editor
   So I can make it easy for users to get back to the top of big pages
 
-  Background:
-    Given I log in as an existing "editor"
+  Scenario Outline: New page
+    Given I log in as an "editor"
+    When I edit a new <content type>
+    And I enable the "Back to top" page setting
+    And I save the page
+    Then I should see a back to top link
 
-  Scenario: Back to top - General page
-    Given I am on "/node/add/document-page"
-    When I fill in "edit-title" with "test"
-    And I fill in "edit-field-title-short-und-0-value" with "test"
-    And I check the box "N/A" in "edit-field-site-section-und"
-    And I click "Menu settings"
-    And I select "About us" from "edit-menu-parent-hierarchical-select-selects-1"
-    And I click "Page settings"
-    And I check "edit-field-setting-backtotop-und"
-    And I press "edit-submit" in the "Submit" region
-    Then I should see "General Page test has been created."
-    And I should see "Back to top" in the "Back to top" region
-
-  Scenario: Back to top - Audit Report
-    Given I go to "/node/add/audit-report"
-    When I fill in "edit-title" with "test"
-    And I fill in "edit-field-title-short-und-0-value" with "test"
-    And I click "Audit"
-    And I select "Amber Valley" from "edit-field-audit-authority-und"
-    And I select "County" from "edit-field-audit-authority-type-und"
-    And I select "Approved establishments audit" from "edit-field-audit-type-und"
-    And I check the box "England" in "edit-field-nation-und"
-    And I click "Page settings"
-    And I check "edit-field-setting-backtotop-und"
-    And I press "edit-submit" in the "Submit" region
-    Then I should see "Audit Report test has been created."
-    And I should see "Back to top" in the "Back to top" region
-
-  Scenario: Back to top - Consultation
-    Given I am on "/node/add/consultation"
-    When I fill in "edit-title" with "test"
-    And I fill in "edit-field-title-short-und-0-value" with "test"
-    And I check the box "N/A" in "edit-field-site-section-und"
-    And I check the box "England" in "edit-field-nation-und"
-    And I click "Page settings"
-    And I check "edit-field-setting-backtotop-und"
-    And I press "edit-submit" in the "Submit" region
-    Then I should see "Consultation test has been created."
-    And I should see "Back to top" in the "Back to top" region
-
-  Scenario: Back to top - FAQ
-    Given I am on "/node/add/faq"
-    When I fill in "edit-title" with "test"
-    And I fill in "edit-field-title-short-und-0-value" with "test"
-    And I check the box "N/A" in "edit-field-site-section-und"
-    And I click "Page settings"
-    And I check "edit-field-setting-backtotop-und"
-    And I press "edit-submit" in the "Submit" region
-    Then I should see "FAQ test has been created."
-    And I should see "Back to top" in the "Back to top" region
-
-  Scenario: Back to top - Job
-    Given I am on "/node/add/job"
-    When I fill in "edit-title" with "test"
-    And I fill in "edit-field-title-short-und-0-value" with "test"
-    And I check the box "N/A" in "edit-field-site-section-und"
-    And I click "Menu settings"
-    And I select "About us" from "edit-menu-parent-hierarchical-select-selects-1"
-    And I click "Page settings"
-    And I check "edit-field-setting-backtotop-und"
-    And I press "edit-submit" in the "Submit" region
-    Then I should see "Job test has been created."
-    And I should see "Back to top" in the "Back to top" region
-
-  Scenario: Back to top - News
-    Given I am on "/node/add/news"
-    When I fill in "edit-title" with "test"
-    And I fill in "edit-field-title-short-und-0-value" with "test"
-    And I click "News *"
-    And I select "General news" from "edit-field-news-type-und"
-    And I click "Feature image *"
-    And I click "Browse"
-    And I wait 5 seconds
-    And I click "Library"
-    And I click "forksaboutusf.jpg"
-    And I click "Submit"
-    And I check the box "N/A" in "edit-field-site-section-und"
-    And I click "Page settings"
-    And I check "edit-field-setting-backtotop-und"
-    And I press "edit-submit" in the "Submit" region
-    Then I should see "News test has been created."
-    And I should see "Back to top" in the "Back to top" region
-
-  Scenario: Back to top - Research project
-    Given I am on "/node/add/project"
-    When I fill in "edit-title" with "test"
-    And I fill in "edit-field-title-short-und-0-value" with "test"
-    And I check the box "N/A" in "edit-field-site-section-und"
-    And I click "Page settings"
-    And I check "edit-field-setting-backtotop-und"
-    And I press "edit-submit" in the "Submit" region
-    Then I should see "Research project test has been created."
-    And I should see "Back to top" in the "Back to top" region
-
-  Scenario: Back to top - Alert
-    Given I am on "/node/add/alert"
-    When I fill in "edit-title" with "test"
-    And I fill in "edit-field-title-short-und-0-value" with "test"
-    And I select "None" from "edit-field-alert-alert-type-und"
-    And I check the box "N/A" in "edit-field-site-section-und"
-    And I click "Page settings"
-    And I check "edit-field-setting-backtotop-und"
-    And I press "edit-submit" in the "Submit" region
-    Then I should see "Alert test has been created."
-    And I should see "Back to top" in the "Back to top" region
+    Examples:
+      | content type     |
+      | general page     |
+      | audit report     |
+      | consultation     |
+      | FAQ entry        |
+      | job listing      |
+      | news post        |
+      | research project |
+      | alert            |
--- tests/features/bootstrap/FeatureContext.php
+++ tests/features/bootstrap/FeatureContext.php
@@ -1,17 +1,16 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Drupal\DrupalExtension\Context\RawDrupalContext;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Drupal\DrupalExtension\Context\MinkContext;
 
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext {
-    private $drupal_users;
-    private $current_user;
-
     /**
      * Initializes context.
      *
@@ -28,317 +27,251 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
     }
 
     /**
-     * @Given I log in as an existing :role
+     * @Given I log in as a(n) :role
      */
-    public function iLogInAsAnExisting($role)
+    public function iLogInAsA($role)
     {
-        $user_pass = $this->fetchUserPassword($role);
-        if ($user_pass) {
-            $login_info = explode('/', $user_pass);
-            if ($login_info && count($login_info) == 2) {
-                $this->user = (object) array(
-                    'name' => $login_info[0],
-                    'pass' => $login_info[1],
-                    'role' => $role,
-                );
-                return $this->userLogin();
-            } else {
-                $err = 'Role not defined properly in behat.yml, expected username/password.';
-            }
-        } else {
-            $err = 'Role not found in behat.yml';
+        $user_pass = $this->drupal_users[$role];
+        if (!$user_pass) {
+            throw new Exception("No $role defined in behat.yml");
         }
 
-        throw new \Exception($err);
+        if (!strpos($user_pass, '/')) {
+            throw new Exception("$role incorrectly specified in behat.yml");
+        }
+
+        list($user, $pass) = explode('/', $user_pass, 2);
+        $this->user = (object) array(
+            'name' => $user,
+            'pass' => $pass,
+            'role' => $role,
+        );
+
+        $this->login();
     }
 
     /**
-     * @When /^I check (?:the box )?"(?P<option>[^"]*)" in "(?P<block>[^"]*)"$/
-     *
-     * @param string $option
-     *   Checkbox option text.
-     * @param string $block
-     *   Block to search, identified by CSS id.
+     * @When I edit a new general page
      */
-    public function iCheckOptionInBlock($option, $block)
-    {
-        $page_element = $this->getSession()->getPage();
-        if (!$page_element) {
-            throw new \Exception('Page not found.');
-        }
-
-        $block_element = $page_element->find('xpath', "//div[@id='$block']");
-        if (!$block_element) {
-            throw new \Exception('Block "' . $block . '" not found.');
-        }
-
-        $block_element->checkField($this->fixStepArgument($option));
-    }
-
-    /**
-     * @When I click on the element with css selector :selector
-     */
-    public function iClickOnTheElementWithCssSelector($selector)
+    public function iEditANewGeneralPage()
     {
         $session = $this->getSession();
-        $element = $session->getPage()->find('xpath',
-            $session->getSelectorsHandler()->selectorToXpath('css', $selector));
-        if ($element === null) {
-            throw new \InvalidArgumentException("Could not evaluate CSS selector \"$selector\"");
+        $session->visit($this->locatePath('/node/add/document-page'));
+
+        if ($this->pageTitle() !== 'Create General Page') {
+            throw new Exception('Error creating new general page');
         }
 
-        $element->click();
+        $this->nodeDefaults();
+
+        $page = $session->getPage();
+
+        $page->findLink('Menu settings')->click();
+        $page->findField('edit-menu-parent-hierarchical-select-selects-1')->selectOption('About us');
     }
 
     /**
-     * @When I wait :n second
-     * @When I wait :n seconds
-     * @When I wait for :n second
-     * @When I wait for :n seconds
+     * @When I enable the :setting page setting
      */
-    public function iWaitForSeconds($n)
+    public function iEnableThePageSetting($setting)
     {
-        usleep($n * 1000000);
+        static $settings = array(
+            'Back to top' => 'edit-field-setting-backtotop-und',
+        );
+
+        $session = $this->getSession();
+
+        $page = $session->getPage();
+        $page->findLink('Page settings')->click();
+        $page->findField($settings[$setting])->click();
     }
 
     /**
-     * @Given I edit the current node
+     * @When I save the page
      */
-    public function iEditTheCurrentNode()
+    public function iSaveThePage()
     {
+        $page = $this->getSession()->getPage();
+        $page->findButton('edit-submit')->click();
 
-        if ($nid = $this->getDrupalNid()) {
-            $this->getSession()->visit($this->locatePath('/node/' . $nid . '/edit'));
-        } else {
-            throw new \Exception('Can\'t get nid.');
-        }
-    }
-
-    /**
-     * @Given I mouse over :link
-     */
-    public function iMouseOver($link)
-    {
-        $page_element = $this->getSession()->getPage();
-        if (!$page_element) {
-            throw new \Exception('Page not found.');
-        }
-        $link_element = $this->findLink($page_element, $link);
-        if (!$link_element) {
-            throw new \Exception('Link "' . $link . '" not found.');
-        }
-        $link_element->mouseOver();
-    }
-
-    /**
-     * @Given I am logged out
-     * @Given I logout
-     * @Given I am anonymous
-     */
-    public function iLogout()
-    {
-        if ($this->loggedIn()) {
-            $this->logout();
-        }
-    }
-
-    /* END STEP FUNCTIONS */
-
-    /**
-     * Return a link by text or class name.
-     *
-     * @param Element $parent
-     *   Element object to search
-     * @param string $link
-     *   Link text, class name or ID to look for.
-     */
-    public function findLink($parent, $link)
-    {
-        $element = $parent->findLink($link);
-        if (!$element) {
-            $element = $parent->find('xpath', "//a[@class='$link']");
-            if (!$element) {
-                $element = $parent->find('xpath', "//a[@id='$link']");
-            }
-        }
-        if ($element) {
-            $this->initVars();
-            $this->vars['link-text'] = $element->getText();
-        }
-        return $element;
-    }
-
-    /**
-     * Helper function to fetch user passwords from behat.yml.
-     *
-     * @param string $type
-     *   The user type, e.g. drupal or git.
-     *
-     * @param string $name
-     *   The role to fetch the password for.
-     *
-     * @return string
-     *   The matching username/password or FALSE on error.
-     */
-    public function fetchUserPassword($name)
-    {
-        try {
-            return $this->drupal_users[$name];
-        } catch (\Exception $e) {
-            throw new \Exception("Non-existent user/password for $name. Please check behat.yml");
+        if (!$page->has('named', array('content', '"has been created"'))) {
+            throw new Exception('error saving new page');
         }
     }
 
     /**
-     * Log in the current user.
+     * @Then I should see a back to top link
      */
-    public function userLogin()
+    public function iShouldSeeABackToTopLink()
     {
-        if (empty($this->user)) {
-            throw new \Exception('User must be set before userLogin.');
-        }
-
-        $user = $this->whoami();
-        if (strtolower($user) == strtolower($this->user->name)) {
-            // Already logged in.
-            return;
-        }
-
-        $page_element = $this->getSession()->getPage();
-        if (empty($page_element)) {
-            throw new \Exception('Page not found.');
-        }
-
-        if ($user != $this->current_user) {
-            $this->logout();
-        }
-
-        $this->getSession()->visit($this->locatePath('/user'));
-        if ($this->isLoginForm()) {
-            // If I see this, I'm not logged in at all, so log in.
-            $this->customLogin();
-            if ($this->loggedIn()) {
-                // Redirect to /user because sometimes we don't wait
-                // for full page load.
-                $this->getSession()->visit($this->locatePath('/user'));
-                return;
-            }
-            throw new \Exception('Not logged in.');
-        }
-        throw new \Exception('Failed to reach the login page.');
-    }
-
-    public function isLoginForm()
-    {
-        $page_element = $this->getSession()->getPage();
-        $button = $page_element->findButton($this->getDrupalText('log_in'));
-        return (bool) $button;
-    }
-
-    public function logout()
-    {
-        $this->getSession()->visit($this->locatePath('/user/logout'));
-    }
-
-    public function whoami()
-    {
-        $page_element = $this->getSession()->getPage();
-        if (!$page_element) {
-            throw new \Exception('Page not found.');
-        }
-
-        try {
-            $data_element = $page_element->find('css', '#behat-data');
-            if ($data_element) {
-                $data = $data_element->getHtml();
-                $matches = array();
-                if (preg_match('/drupal username:(\w*)/', $data, $matches)) {
-                    return $matches[1];
-                }
-            }
-        } catch (\Exception $e) {
-            echo 'Caught exception: ', $e->getMessage(), '\n';
-        }
-
-        return $this->current_user;
-    }
-
-    public function customLogin()
-    {
-        $page_element = $this->getSession()->getPage();
-        $page_element->fillField($this->getDrupalText('username_field'), $this->user->name);
-        $page_element->fillField($this->getDrupalText('password_field'), $this->user->pass);
-        $submit = $page_element->findButton($this->getDrupalText('log_in'));
-        if (!$submit) {
-            throw new \Exception('No submit button on "' . $this->getSession()->getCurrentUrl() . '".');
-        }
-        $submit->click();
-    }
-
-    protected function fixStepArgument($argument)
-    {
-        $argument = str_replace('\\"', '"', $argument);
-        $this->initVars();
-        static $random = array();
-        for ($start = 0;
-             ($start = strpos($argument, '[', $start)) !== FALSE;
-             /* omitted */  ) {
-            $end = strpos($argument, ']', $start);
-            if ($end === FALSE) {
-                break;
-            }
-            $name = substr($argument, $start + 1, $end - $start - 1);
-            if ($name == 'random') {
-                $this->vars[$name] = Random::name(8);
-                $random[] = $this->vars[$name];
-            } elseif (substr($name, 0, 7) == 'random:') {
-                $num = substr($name, 7);
-                if (is_numeric($num) && $num <= count($random)) {
-                    $this->vars[$name] = $random[count($random) - $num];
-                }
-            }
-            if (isset($this->vars[$name])) {
-                $argument = substr_replace($argument, $this->vars[$name], $start, $end - $start + 1);
-                $start += strlen($this->vars[$name]);
-            } else {
-                $start = $end + 1;
-            }
-        }
-
-        return $argument;
-    }
-
-    private function initVars()
-    {
-        if (!isset($this->vars['host'])) {
-            $this->vars['host'] = parse_url($this->getSession()->getCurrentUrl(), PHP_URL_HOST);
+        $page = $this->getSession()->getPage();
+        if (!$page->has('css', '.section-back-top > a')) {
+            throw new Exception('Back to top link not found');
         }
     }
 
-    private function getDrupalNid()
+    /**
+     * @When I edit a new audit report
+     */
+    public function iEditANewAuditReport()
     {
-        $nid = null;
-        $data = $this->getDrupalData();
-        $matches = array();
-        if (preg_match('/drupal nid:(\w*)/', $data, $matches)) {
-            if (!$nid = $matches[1]) {
-                throw new \Exception('Can\'t get nid.');
-            }
+        $session = $this->getSession();
+        $session->visit($this->locatePath('/node/add/audit-report'));
+
+        if ($this->pageTitle() !== 'Create Audit Report') {
+            throw new Exception('Error creating new audit report');
         }
-        return $nid;
+
+        $this->nodeDefaults();
+
+        $page = $session->getPage();
+        $page->findLink('Audit')->click();
+        $page->findField('Audit type')->selectOption('Approved establishments audit');
+        $page->findField('Authority')->selectOption('Amber Valley');
+        $page->findField('Authority Type')->selectOption('County');
     }
 
-    private function getDrupalData()
+    /**
+     * @When I edit a new consultation
+     */
+    public function iEditANewConsultation()
     {
-        $page_element = $this->getSession()->getPage();
-        $page_url = $this->getSession()->getCurrentUrl();
-        if (!$page_element) {
-            throw new \Exception('Page not found.');
+        $session = $this->getSession();
+        $session->visit($this->locatePath('/node/add/consultation'));
+
+        if ($this->pageTitle() !== 'Create Consultation') {
+            throw new Exception('Error creating new consultation');
         }
 
-        $data_element = $page_element->find('css', '#behat-data');
-        if ($data_element) {
-            return $data_element->getHtml();
+        $this->nodeDefaults();
+    }
+
+    /**
+     * @When I edit a new FAQ entry
+     */
+    public function iEditANewFaqEntry()
+    {
+        $session = $this->getSession();
+        $session->visit($this->locatePath('/node/add/faq'));
+
+        if ($this->pageTitle() !== 'Create FAQ') {
+            throw new Exception('Error creating new FAQ entry');
         }
+
+        $this->nodeDefaults();
+
+        $page = $session->getPage();
+
+        $page->findLink('Menu settings')->click();
+        $page->findField('edit-menu-enabled')->check();
+        $page->findField('edit-menu-parent-hierarchical-select-selects-1')->selectOption('About us');
+    }
+
+    /**
+     * @When I edit a new job listing
+     */
+    public function iEditANewJobListing()
+    {
+        $session = $this->getSession();
+        $session->visit($this->locatePath('/node/add/job'));
+
+        if ($this->pageTitle() !== 'Create Job') {
+            throw new Exception('Error creating new job listing');
+        }
+
+        $this->nodeDefaults();
+
+        $page = $session->getPage();
+        $page->findLink('Menu settings')->click();
+        $page->findField('edit-menu-parent-hierarchical-select-selects-1')->selectOption('About us');
+    }
+
+    /**
+     * @When I edit a new news post
+     */
+    public function iEditANewNewsPost()
+    {
+        throw new PendingException();
+        // TODO: figure out how to attach a feature image
+        $session = $this->getSession();
+        $session->visit($this->locatePath('/node/add/news'));
+
+        if ($this->pageTitle() !== 'Create News') {
+            throw new Exception('Error creating new news post');
+        }
+
+        $this->nodeDefaults();
+
+        $page = $session->getPage();
+        $page->findLink('News *')->click();
+        $page->findField('News type')->selectOption('General news');
+        $page->findLink('Feature image *')->click();
+        $page->findLink('Browse')->click();
+        $session->wait(5000);
+        $page->findLink('Library')->click();
+        $page->find('css', '.media-item img')->click();
+        $page->find('css', '.ui-dialog')->findLink('submit')->click();
+    }
+
+    /**
+     * @When I edit a new research project
+     */
+    public function iEditANewResearchProject()
+    {
+        $session = $this->getSession();
+        $session->visit($this->locatePath('/node/add/project'));
+
+        if ($this->pageTitle() !== 'Create Research Project') {
+            throw new Exception('Error creating new research project');
+        }
+
+        $this->nodeDefaults();
+    }
+
+    /**
+     * @When I edit a new alert
+     */
+    public function iEditANewAlert()
+    {
+        $session = $this->getSession();
+        $session->visit($this->locatePath('/node/add/alert'));
+
+        if ($this->pageTitle() !== 'Create Alert') {
+            throw new Exception('Error creating new alert');
+        }
+
+        $this->nodeDefaults();
+    }
+
+    public function pageTitle()
+    {
+        $page = $this->getSession()->getPage();
+        $title = $page->findById('page-title');
+        if (!$title) {
+            $title = $page->find('css', '.page-title');
+        }
+        return $title->getText();
+    }
+
+    public function loggedIn()
+    {
+        // parent::loggedIn() doesn't work when the admin menu doesn't
+        // show up, so try a different check
+        $session = $this->getSession()->visit($this->locatePath('/user'));
+        return $this->pageTitle() === $this->user->name;
+    }
+
+    /**
+     * Set up some common defaults for creating a new node.
+     */
+    public function nodeDefaults()
+    {
+        $page = $this->getSession()->getPage();
+
+        $page->findField('edit-title')->setValue('test');
+        $page->findField('edit-field-title-short-und-0-value')->setValue('test');
+        $page->findLink('Audience')->click();
+        $page->findField('England')->check();
+        $page->findField('edit-field-site-section-und-15')->check();
     }
 }
