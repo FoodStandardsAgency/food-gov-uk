26a3968a62a7cbc115f304adf75163724a4439cc
Matt Farrow <matt.farrow@siriusopensource.com>
2015-07-31 13:48:45 +0100

    Remove empty ShareThis button containers
    Using JavaScript, we wait until ShareThis has fully loaded and
    initialised, then we check for and remove any buttons that do not
    contain any `<span>` elements as children.
    
    This fixes the issue where an empty button appears for WhatsApp
    on non-mobile devices.
    
    Tested on Chrome, Firefox, IE9, Opera, Safari.
    
    [ Partial fix for #10159 ]

--- docroot/sites/all/modules/custom/fsa_sharethis/js/fsa-sharethis.js
+++ docroot/sites/all/modules/custom/fsa_sharethis/js/fsa-sharethis.js
@@ -11,6 +11,45 @@
         button.click();
         e.preventDefault();
       });
+      // Remove any empty buttons
+      this.removeEmptyButtons();
+    },
+
+    // Checks to see whether ShareThis has fully loaded.
+    // To determine whether this is the case, we check to see whether the
+    // button elements have the `st_processed` attribute. If so, ShareThis has
+    // finished initialising.
+    checkShareThisLoaded : function() {
+      $button = $('.sharethis-wrapper button:first');
+      if ($button.attr('st_processed')) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    },
+
+    // Removes button containers that are not in use.
+    // This happens, for instance, when WhatsApp is one of the chosen services,
+    // but we're viewing the page on a non-mobile device. We use the
+    // `checkShareThisLoaded` method to determine whether ShareThis has finished
+    // its initialisation. If so, we remove any button containers that don't
+    // have any `<span>` elements as children. If ShareThis has not loaded, we
+    // wait 1/100th of a second and try again until it has loaded.
+    removeEmptyButtons : function() {
+      var st = this;
+      if (!this.checkShareThisLoaded()) {
+        window.setTimeout(function(){st.removeEmptyButtons()}, 10);
+      }
+      else {
+        $buttons = $('.sharethis-wrapper button');
+        $buttons.each(function(i){
+          if ($(this).find('span').length == 0) {
+            $(this).remove();
+          }
+        });
+      }
     }
+
   }
 })(jQuery);
