70225afea513e842e63e103f75b04cd4c6de7c6d
Matt Farrow <matt.farrow@siriusopensource.com>
2015-11-16 16:43:58 +0000

    Modify suggestions template to show suggestions
    The suggestions template now shows a link to the original query
    or a suggestion, depending on the search results.
    
    [ Partial fix for #10176 ]

--- docroot/sites/all/modules/custom/fsa_search_spellcheck/theme/search-spelling-suggestions.tpl.php
+++ docroot/sites/all/modules/custom/fsa_search_spellcheck/theme/search-spelling-suggestions.tpl.php
@@ -5,11 +5,17 @@
  */
 ?>
 
-<?php if ($show_suggestions): ?>
+<?php if ($show_original): ?>
   <div class="search-spelling-suggestions">
     <p>Showing results for <strong><?php print render($keyword); ?></strong>.
     <?php if (!empty($original_query)): ?>
       Search instead for <a href="<?php print $original_query_link; ?>"><?php print render($original_query); ?></a>.</p>
     <?php endif; ?>
+  </div>
+<?php endif; ?>
+
+<?php if ($show_suggestions): ?>
+  <div class="search-spelling-suggestions">
+    <p>Did you mean <a href="<?php print $suggested_query_link; ?>"><?php print render($suggestion); ?></a>?</p>
   </div>
 <?php endif; ?>
