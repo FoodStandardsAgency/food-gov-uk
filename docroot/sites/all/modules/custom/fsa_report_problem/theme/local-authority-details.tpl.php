0714f399bffa8d89ca9a71722d0f3e912f75a7c6
Matt Farrow <matt.farrow@siriusopensource.com>
2015-11-30 10:53:27 +0000

    Clean up local authority details template
    * Added content attributes div
    * Removed hard-coded wrapper class - now uses default class
    * Removed intro - now in the form
    
    [ Partial fix for #10273 ]

--- docroot/sites/all/modules/custom/fsa_report_problem/theme/local-authority-details.tpl.php
+++ docroot/sites/all/modules/custom/fsa_report_problem/theme/local-authority-details.tpl.php
@@ -4,15 +4,16 @@
  * Default theme implementation for local authority details.
  */
 ?>
-<div class="local-authority-details">
-  <p><?php print t('The local authority for this business is:'); ?></p>
-  <?php if (!empty($name)): ?>
-    <h2><?php print render($name); ?></h2>
-  <?php endif; ?>
-  <?php if (!empty($email_link)): ?>
-    <p>Email address: <?php print render($email_link); ?></p>
-  <?php endif; ?>
-  <?php if (!empty($website_link)): ?>
-    <p>Website: <?php print render($website_link); ?></p>
-  <?php endif; ?>
+<div <?php print $attributes; ?>>
+  <div <?php print $content_attributes; ?>>
+    <?php if (!empty($name)): ?>
+      <h2><?php print render($name); ?></h2>
+    <?php endif; ?>
+    <?php if (!empty($email_link)): ?>
+      <p>Email address: <?php print render($email_link); ?></p>
+    <?php endif; ?>
+    <?php if (!empty($website_link)): ?>
+      <p>Website: <?php print render($website_link); ?></p>
+    <?php endif; ?>
+  </div>
 </div>
