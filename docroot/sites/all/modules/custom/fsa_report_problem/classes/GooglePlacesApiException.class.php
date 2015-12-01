a4639622d888a223824fbf3a77edd4269d12ed24
Matt Farrow <matt.farrow@siriusopensource.com>
2015-12-01 11:36:30 +0000

    Add class for GooglePlacesApiException
    Added a new class GooglePlacesApiException, which extends the standard
    PHP Exception class.
    
    This is used to throw exceptions if there are problems with the Google
    Places API lookup.
    
    [ Partial fix for #10273 ]

---
+++ docroot/sites/all/modules/custom/fsa_report_problem/classes/GooglePlacesApiException.class.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @file
+ * Class file for GooglePlacesApiException class
+ */
+
+/**
+ * Error codes
+ */
+
+/**
+ * No internet connection avaliable
+ */
+define('GOOGLE_PLACES_EXCEPTION_NO_CONNECTION', 0);
+
+/**
+ * No API key available
+ */
+define('GOOGLE_PLACES_EXCEPTION_NO_API_KEY', 1);
+
+/**
+ * Over query limit
+ */
+define('GOOGLE_PLACES_EXCEPTION_OVER_QUERY_LIMIT', 2);
+
+/**
+ * Request denied - invalid key
+ */
+define('GOOGLE_PLACES_EXCEPTION_INVALID_KEY', 3);
+
+/**
+ * HTTP error
+ */
+define('GOOGLE_PLACES_EXCEPTION_HTTP_ERROR', 4);
+
+/**
+ * Unspecified error
+ */
+define('GOOGLE_PLACES_EXCEPTION_UNSPECIFIED', 5);
+
+/**
+ * GooglePlacesApiException class.
+ */
+class GooglePlacesApiException extends Exception {
+  // Class constructor method - overrides parent
+  function __construct($message, $code = 0, $http_code = NULL, $previous = NULL) {
+    // Call the parent constructor method with the standard parameters
+    parent::__construct($message, $code, $previous);
+    // Add the HTTP code to the message
+    $this->setMessage($this->message, $http_code);
+  }
+
+  // Set message function - creates a custom exception message.
+  private function setMessage($message, $http_code = NULL) {
+    $this->message = $this->getCode() . " - " . $message;
+    if (!is_null($http_code)) {
+      $this->message .= " (HTTP code: $http_code)";
+    }
+  }
+}
