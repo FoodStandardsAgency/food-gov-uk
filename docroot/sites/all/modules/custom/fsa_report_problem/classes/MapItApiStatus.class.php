10dcf18a9bc8464f3a779c804a5cc7912aa7c754
Matt Farrow <matt.farrow@siriusopensource.com>
2015-12-18 16:04:13 +0000

    Get MapIt URL from function - if available
    In the MapItApiStatus class, we now get the test URL from the
    helper function for generating MapIt URLs - if it is available.
    
    [ Partial fix for #10273 ]

--- docroot/sites/all/modules/custom/fsa_report_problem/classes/MapItApiStatus.class.php
+++ docroot/sites/all/modules/custom/fsa_report_problem/classes/MapItApiStatus.class.php
@@ -7,7 +7,7 @@
 class MapItApiStatus extends ExternalApiStatus {
   function __construct() {
     parent::__construct();
-    $this->url = 'https://mapit.mysociety.org/postcode/SW1A1AA';
+    $this->url = is_callable('_fsa_report_problem_mapit_url') ? _fsa_report_problem_mapit_url('postcode/SW1A1AA') : 'https://mapit.mysociety.org/postcode/SW1A1AA';
     $this->name = 'MapIt API';
   }
 }
