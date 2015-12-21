6c28e436b534601dec6ff369664a37fd88a09113
Matt Farrow <matt.farrow@siriusopensource.com>
2015-12-21 10:29:22 +0000

    Use options in MapIt API status check
    When checking the status of the MapIt API, we now pass in
    the options, including user agent string.
    
    [ Partial fix for #10273 ]

--- docroot/sites/all/modules/custom/fsa_report_problem/classes/MapItApiStatus.class.php
+++ docroot/sites/all/modules/custom/fsa_report_problem/classes/MapItApiStatus.class.php
@@ -8,6 +8,7 @@ class MapItApiStatus extends ExternalApiStatus {
   function __construct() {
     parent::__construct();
     $this->url = is_callable('_fsa_report_problem_mapit_url') ? _fsa_report_problem_mapit_url('postcode/SW1A1AA') : 'https://mapit.mysociety.org/postcode/SW1A1AA';
+    $this->checkOptions['headers']['User-Agent'] = _fsa_report_problem_mapit_user_agent();
     $this->name = 'MapIt API';
   }
 }
