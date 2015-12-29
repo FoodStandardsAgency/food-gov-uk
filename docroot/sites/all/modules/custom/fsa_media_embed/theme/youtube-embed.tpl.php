2f054c84eef54a085ecff90ec8714e1b0cbb22d1
Matt Farrow <matt.farrow@siriusopensource.com>
2015-12-29 13:25:49 +0000

    Allow Vimeo embeds
    Set up the architecture of the module to allow embeds
    from multiple sources, starting with Vimeo
    
    @todo Make embedding more generic and easier to extend
    @todo Make the generic media embed template useful
    
    [ Partial fix for #10184 ]

--- docroot/sites/all/modules/custom/fsa_media_embed/theme/youtube-embed.tpl.php
+++ docroot/sites/all/modules/custom/fsa_media_embed/theme/youtube-embed.tpl.php
@@ -1,5 +1,5 @@
 <div class="embed-wrapper">
   <div <?php print $attributes; ?>>
-    <iframe <?php print $iframe_attributes; ?> src="https://www.youtube.com/embed/<?php print $youtube_id; ?>"></iframe>
+    <iframe <?php print $iframe_attributes; ?> src="https://www.youtube.com/embed/<?php print $media_id; ?>"></iframe>
   </div>
 </div>
