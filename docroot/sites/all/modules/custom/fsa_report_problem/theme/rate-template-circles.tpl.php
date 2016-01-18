e82de41a5f15e089a0460d83f2f1a0d588979b7d
Matt Farrow <matt.farrow@siriusopensource.com>
2016-01-18 11:42:47 +0000

    Add template for circle rating widget
    [ Partial fix for #10273 ]

---
+++ docroot/sites/all/modules/custom/fsa_report_problem/theme/rate-template-circles.tpl.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file
+ * Default theme implementation for TripAdvisor-style circles rating widget
+ */
+?>
+
+<?php if ($display_options['description']): ?>
+  <div class="rate-description">
+    <?php print $display_options['description']; ?>
+  </div>
+<?php endif; ?>
+
+<?php print drupal_render($circles); ?>
+  
+  <div id="rate-value-description"></div>
+
+<?php if (!empty($info)): ?>
+  <div class="rate-info"><?php print $info; ?></div>
+<?php endif; ?>
