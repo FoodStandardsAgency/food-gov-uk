f0353848397a75501594a0967e57eefcf86e046b
Matt Farrow <matt.farrow@siriusopensource.com>
2016-01-20 12:05:01 +0000

    Amended JavaScript for circle ratings for new CSS
    Amended the way the JavaScript for the circle ratings widget works
    to accommodate the new CSS-based approach.
    
    [ Partial fix for #10273 ]

--- docroot/sites/all/modules/custom/fsa_report_problem/js/circles.js
+++ docroot/sites/all/modules/custom/fsa_report_problem/js/circles.js
@@ -12,15 +12,17 @@
             {
                 // Append rate-circles-btn-filled class to all the a-elements except the a-elements after the hovered element
                 var $this = $(this);
-                $this.siblings().children('a').addClass('rate-circles-btn-filled').removeClass('rate-circles-btn-empty');
-                $this.children('a').addClass('rate-circles-btn-filled').removeClass('rate-circles-btn-empty');
-                $this.nextAll().children('a').removeClass('rate-circles-btn-filled').addClass('rate-circles-btn-empty');
+                $this.siblings().children('a').removeClass('rate-circles-btn-filled').addClass('rate-circles-btn-empty');
+                $this.prevAll().addClass('filled');
+                $this.prevAll().children('a').addClass('rate-circles-btn-filled').removeClass('rate-circles-btn-empty');
                 $('#rate-value-description').text($this.children('a').attr('title'));
             },
             function()
             {
                 // Restore the current vote status
-                $(this).parent().children().children('a').removeClass('rate-circles-btn-filled').addClass('rate-circles-btn-empty');
+                var $this = $(this);
+                $this.siblings().removeClass('filled');
+                $this.siblings().children('a').removeClass('rate-circles-btn-filled').addClass('rate-circles-btn-empty');
                 $(this).parent().children().slice(0,status).children('a').removeClass('rate-circles-btn-empty').addClass('rate-circles-btn-filled');
                 $('#rate-value-description').text('');
             }
