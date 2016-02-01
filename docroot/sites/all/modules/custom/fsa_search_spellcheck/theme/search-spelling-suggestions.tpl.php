06863125aee407a44904b802faad40e00d1a92a6
Matt Farrow <matt.farrow@siriusopensource.com>
2016-02-01 11:22:47 +0000

    Amend search spelling suggestions template
    Now handles various different scenarios.
    
    Old code is still in there but commented out in HTML.
    
    @todo Remove the old code
    @todo Make sure text is translatable
    @todo Make sure all scenarios are catered for
    @todo Make text editable via admin interface
    
    [ Partial fix for #10176 ]

--- docroot/sites/all/modules/custom/fsa_search_spellcheck/theme/search-spelling-suggestions.tpl.php
+++ docroot/sites/all/modules/custom/fsa_search_spellcheck/theme/search-spelling-suggestions.tpl.php
@@ -5,6 +5,47 @@
  */
 ?>
 
+<!--
+<?php if (!empty($suggested_keyword_links)): ?>
+  <?php foreach ($suggested_keyword_links as $link): ?>
+    <?php print render($link); ?>
+  <?php endforeach; ?>
+<?php endif; ?>
+-->
+
+<?php if ($ch > $hits): ?>
+  <div class="search-spelling-suggestions">
+    <?php if ($hits > 0): ?>
+      <p>Showing results for <strong><?php print render($keyword); ?></strong>. Search instead for
+        <?php if ($cs): ?>
+          <a href="<?php print $suggested_query_link; ?>"><?php print render($suggestion); ?></a>.
+        <?php else: ?>
+          <a href="<?php print $original_query_link; ?>"><?php print render($original_query); ?></a>.
+        <?php endif; ?>
+      </p>
+    <?php else: ?>
+      <p>A search for <em><?php print render($original_query); ?></em> returned no results. Found <?php print render($ch); ?> results by searching for <strong><?php print render($keyword); ?></strong> instead.</p>
+    <?php endif; ?>
+  </div>
+<?php elseif ($ch > 0 && !empty($suggestion)): ?>
+  <div class="search-spelling-suggestions">
+    <p>Showing results for <strong><?php print render($keyword); ?></strong>. You may alternatively want to try a search for <a href="<?php print $suggested_query_link; ?>"><?php print render($suggestion); ?></a>.</p>
+  </div>
+<?php endif; ?>
+
+
+
+
+
+
+<!--
+<hr>
+
+OLD STUFF BELOW
+
+<hr>
+
+
 <?php if ($show_original): ?>
   <div class="search-spelling-suggestions">
     <p>Showing results for <strong><?php print render($keyword); ?></strong>.
@@ -19,3 +60,5 @@
     <p>Did you mean <a href="<?php print $suggested_query_link; ?>"><?php print render($suggestion); ?></a>?</p>
   </div>
 <?php endif; ?>
+
+-->
