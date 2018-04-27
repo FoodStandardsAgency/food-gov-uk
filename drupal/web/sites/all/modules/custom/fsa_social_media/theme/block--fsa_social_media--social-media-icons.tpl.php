4d8f7fb7e06949e63f6c9fa4b5a80d171f1d4c7a
Lauris <lauris.igaunis@wunderkraut.com>
2018-04-27 11:34:06 +0300

    Updated "Stay updated" links

--- drupal/web/sites/all/modules/custom/fsa_social_media/theme/block--fsa_social_media--social-media-icons.tpl.php
+++ drupal/web/sites/all/modules/custom/fsa_social_media/theme/block--fsa_social_media--social-media-icons.tpl.php
@@ -79,9 +79,9 @@
         <div class="icons-wrapper">
         <ul>
           <?php if (!$maintenance_mode): ?>
-            <li><a href="/subscribe" target="_blank" title="email"><img alt="email" src="/sites/all/themes/site_frontend/images/icons/34x34/email.png" /></a></li>
-            <li><a href="/subscribe" target="_blank" title="phone"><img alt="phone" src="/sites/all/themes/site_frontend/images/icons/34x34/phone.png" /></a></li>
-            <li class="extra-margin"><a href="/about-us/data-and-policies/aboutsite/rss" target="_blank" title="RSS"><img alt="RSS" src="/sites/all/themes/site_frontend/images/icons/34x34/rss.png" /></a></li>
+            <li><a href="https://www.food.gov.uk/news-alerts" target="_blank" title="email"><img alt="email" src="/sites/all/themes/site_frontend/images/icons/34x34/email.png" /></a></li>
+            <li><a href="https://www.food.gov.uk/news-alerts" target="_blank" title="phone"><img alt="phone" src="/sites/all/themes/site_frontend/images/icons/34x34/phone.png" /></a></li>
+            <li class="extra-margin"><a href="https://www.food.gov.uk/news-alerts" target="_blank" title="RSS"><img alt="RSS" src="/sites/all/themes/site_frontend/images/icons/34x34/rss.png" /></a></li>
           <?php endif; ?>
           <?php foreach($items as $item): ?>
             <li class="<?= $item['classes']; ?>"><a href="<?php print render($item['url']); ?>" target="_blank" title="<?php print render($item['title']); ?>"><?php print render($item['icon']); ?></a></li>
